d2720d976849af0e13842cf5346447946583b859
Yael Turgeman <yael.turgeman@post.runi.ac.il>
2024-12-01 18:19:29 +0200

    done

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -36,30 +36,47 @@ public class ArrCharOps {
     /** Returns the char value at the specified index. Assume that the array is non-empty.
      */
     public static char charAt(char[] arr, int index) {
-        // Replace the following statement with your code
-        return 0;
+        return arr[index];
     }
 
     /** If the two arrays have the same value in every index, 
      *  returns true; Otherwise returns false.
      */
     public static boolean equals(char[] arr1, char[] arr2) {
-        // Replace the following statement with your code
-        return false;
+        for (int i = 0; i<arr1.length; i++)
+        {
+            if(arr1[i]!=arr2[i]){
+                return false;
+            }
+        }
+        return true;
+        
     }
 
     /** Returns the index within the given array of the first occurrence of the given character.
      *  If no such character is found, returns -1.
      */
     public static int indexOf(char[] arr, char ch) {
-        // Replace the following statement with your code
+        for(int i = 0; i< arr.length; i++)
+        {
+            if(arr[i] == ch)
+            {
+                return i;
+            }
+        }
         return -1;
     }
 
     /** Same as indexOf(char[], char), but starts the search in the given index.
      */
     public static int indexOf(char[] arr, char ch, int fromIndex) {
-        // Replace the following statement with your code
+        for(int i = fromIndex; i< arr.length; i++)
+        {
+            if(arr[i] == ch)
+            {
+                return i;
+            }
+        }
         return -1;
     }
 
@@ -67,15 +84,26 @@ public class ArrCharOps {
      *  If no such character is found, returns -1.
      */
     public static int lastIndexOf(char[] arr, char ch) {
-        // Replace the following statement with your code
+        for (int i = arr.length-1; i >= 0; i--) { 
+            if (arr[i] == ch) {
+                return i; 
+            }
+        }
         return -1;
     }
 
     /* Returns an array which is the concatanation of the two given arrays.
     */
     public static char[] concat(char[] arr1, char[] arr2) {
-        // Replace the following statement with your code
-        return null;
+        int newLength = arr1.length + arr2.length;
+        char[] result = new char[newLength];
+        for (int i = 0; i < arr1.length; i++) {
+            result[i] = arr1[i];
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            result[arr1.length + i] = arr2[i];
+        }
+        return result;
     }
 
     /** Returns a new array that can be described as a sub-array of this array.
@@ -84,8 +112,12 @@ public class ArrCharOps {
      *  characters containing the characters "urge".
      */     
     public static char[] subArray(char[] arr, int beginIndex, int endIndex) {
-        // Replace the following statement with your code
-        return null;
+
+        char[] result = new char[endIndex-beginIndex];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = arr[beginIndex + i];
+        }
+        return result;
     }
 
      /** Returns a single integer that represents the given array. This integer is sometimes 
@@ -96,8 +128,12 @@ public class ArrCharOps {
      *  The hash value of an empty array is zero.
      */
     public static long hashCode(char[] arr) {
-        // Replace the following statement with your code
-        return 0;
+        int hash = 0;
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            hash += arr[i] * Math.pow(7, n - 1 - i);
+        }
+        return hash;
     }
 
     /**
@@ -126,7 +162,26 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        // Replace the following statement with your code
+        if (str1 == null || str2 == null)
+        {
+            return -2;
+        }
+        int minLength = Math.min(str1.length(), str2.length());
+        for (int i = 0; i < minLength; i++) {
+            char char1 = str1.charAt(i);
+            char char2 = str2.charAt(i);
+    
+            if (char1 < char2) {
+                return -1; 
+            } else if (char1 > char2) {
+                return 1; 
+            }
+        }
+        if (str1.length() < str2.length()) {
+            return -1; 
+        } else if (str1.length() > str2.length()) {
+            return 1; 
+        }
         return 0;
     }
 }
--- Primes.java
+++ Primes.java
@@ -1,5 +1,30 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        int counter = 0;
+        boolean[] isPrime = new boolean[n+1];
+        isPrime[0] = false;
+        isPrime[1] = false;
+        for(int i = 2; i<isPrime.length; i++)
+        {
+            isPrime[i]=true;
+        }
+        for (int i = 2; i <=Math.sqrt(n); i++) {
+            if (isPrime[i]) { 
+                for (int j = i * 2; j <= (n); j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        System.out.println("Prime numbers up to " + n + ":") ;
+        for (int i = 0; i < isPrime.length; i++) {
+            if (isPrime[i]) { 
+                counter ++;
+                System.out.println (i);
+            }
+        }
+        int percent = counter * 100 / n;
+        System.out.println("There are " + counter + " primes between 2 and "                                           
+                          + n + " (" + percent + "% are primes)");
+     }   
     }
-}
